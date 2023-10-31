d0c7fe608e77668a1d71db2968abcd36487ea059
kagerer19 <kagerer19@gmail.com>
2023-10-31 19:43:51 +0100

    Add unit tests for personal management.

---
+++ src/test/java/PMTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PMTest {
+    @Test
+    void InvalidNameException() {}
+
+    @Test
+    void testCreateUser() {}
+
+    @Test
+    void testCreateAddress() {}
+
+    void NullpointerException() {}
+
+    void testFindUserByName() {}
+}
+
+//Tests
+//If a user creates a person with a number in the name, an "InvalidPersonName-Exception" should be thrown.
+//The stacktrace of this exception should be displayed in the main.
+//The program shall not terminate and receive further inputs.
+//Test
